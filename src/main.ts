390ab11f5450eb972060e9318a272b0aeba7066e
sarabdev <sarab@contrivers.dev>
2024-02-18 20:03:00 +0500

    added cors url

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,14 @@ import { SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true, logger: ['error', 'warn'] });
   
+  app.enableCors({
+    origin:"https://app-stage.trackrec.co"
+  })
+
    //swagger
    const document = SwaggerModule.createDocument(app, swaggerConfig);
    SwaggerModule.setup("api", app, document);
 
-   app.enableCors()
   //Pine Logger
   app.useLogger(app.get(Logger));
   
