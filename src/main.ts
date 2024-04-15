2345d1f7f50f7436c2a067acabb88278d40d3d53
sarabdev <sarab@contrivers.dev>
2024-04-15 19:25:02 +0500

    added sentry

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from "nestjs-pino";
 import { swaggerConfig } from "./config/swagger.config";
 import { SwaggerModule } from "@nestjs/swagger";
-
+import * as Sentry from '@sentry/node';
+import { SentryFilter } from './sentry.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true, logger: ['error', 'warn'] });
   
   app.enableCors()
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+  });
+
+
+  // Import the filter globally, capturing all exceptions on all routes
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
 
    //swagger
    const document = SwaggerModule.createDocument(app, swaggerConfig);
