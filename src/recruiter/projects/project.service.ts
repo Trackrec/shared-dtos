e16d6038dabeb74d32624e2a7320676cc3acb9a1
Sarab Ali <126464788+sarabdev@users.noreply.github.com>
2024-09-19 11:49:29 +0500

    checking sum should be 100

--- src/recruiter/projects/project.service.ts
+++ src/recruiter/projects/project.service.ts
@@ -451,11 +451,14 @@ export class RecruiterProjectService {
       project.existing_business_range !== null &&
       project.partnership_range !==null &&
       project.business_range !== null &&
+      ((project.business_range + project.existing_business_range + project.partnership_range)==100) &&
       project.inbound_range !== null &&
+      ((project.inbound_range + project.outbound_range)==100) &&
       project.outbound_range !== null &&
       project.smb !== null &&
       project.midmarket !== null &&
       project.enterprise !== null &&
+      ((project.smb + project.midmarket + project.enterprise)==100) &&
       project.minimum_deal_size !== null &&
       project.minimum_sale_cycle !== null &&
       project.Industry_Works_IN &&
