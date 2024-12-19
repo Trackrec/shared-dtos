7a23008efda80423f2518f45e37ca1573b6d7606
Sarab Ali <126464788+sarabdev@users.noreply.github.com>
2024-12-19 15:32:01 +0500

    add exact_location field in project

--- src/recruiter/projects/project.entity.ts
+++ src/recruiter/projects/project.entity.ts
@@ -156,6 +156,9 @@ export class RecruiterProject {
     @Column({ type: 'longtext', nullable: true })
     elevator_pitch: string;
 
+    @Column({ nullable: true })
+    exact_location: string;
+
     @Column({ type: 'longtext', nullable: true })
     travel_requirement_percentage: string;
 
