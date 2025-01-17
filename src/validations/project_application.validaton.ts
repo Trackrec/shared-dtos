04e8388831f3d3c4b1f302c062b53716d38aa432
Sarab Ali <126464788+sarabdev@users.noreply.github.com>
2025-01-17 11:19:29 +0500

    update validation for project application

--- src/validations/project_application.validaton.ts
+++ src/validations/project_application.validaton.ts
@@ -4,10 +4,6 @@ export const projectApplicationRequestSchema = z.object({
   project_id: z.union([z.string(), z.number()]),
   ote: z.number().positive('OTE must be a positive number.'),
   available: z.boolean(),
-  position_id: z
-    .number()
-    .int('Position ID must be an integer.')
-    .positive('Position ID must be a positive number.'),
-  city: z.string().min(1, 'City is required.'),
+  position_id: z.union([z.string(), z.number()]),
   custom_current_role: z.string().min(1, 'Custom current role is required.'),
 });
